a2b0c1719a805301415fe52126c2e5f395773b2f
Benson Kalahar <bensonk42@gmail.com>
2010-05-12 22:59:12 -0700

    Made the meat of the app. Needs to make passwords.
    The body of the app is there, including the gratuitous ajax, buttons,
    and less stylesheet.  Now we just need some fancy logic to generate
    passwords and we'll be good to go, eh?

--- pass.rb
+++ pass.rb
@@ -1,6 +1,40 @@
+#!/usr/bin/env ruby
 require 'rubygems'
 require 'sinatra'
+require 'erb'
+require 'less'
+
+helpers do 
+  def easy
+    "password"
+  end
+
+  def medium
+    "drowssap"
+  end
+
+  def hard
+    "p4s5w0rd!"
+  end
+end
+
+get '/screen.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  less :screen
+end
+
+get '/easy' do
+  easy
+end
+
+get '/medium' do
+  medium
+end
+
+get '/hard' do
+  hard
+end
 
 get '/' do
-  "Welcome to the password generator!"
+  erb :index
 end
